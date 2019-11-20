03f118eb59f2ede93a00050bca12e56949c1974e
Seth Nielsen <sethmnielsen@gmail.com>
2019-11-20 17:16:16 -0600

    Auto-pushed changes

--- importing.py
+++ importing.py
@@ -10,12 +10,20 @@ try:
 except ImportError:
     pass
 
-np.set_printoptions(precision=4, suppress=True, sign=' ', linewidth=120)
+np.set_printoptions(precision=4, suppress=True, sign=' ', linewidth=100)
 
 
 def npaddr(x):
     # Returns the memory block address of an array x.
     return x.__array_interface__['data'][0]
+
+def mem_layout(x):
+    # Prints the values of x in the order they appear in memory
+    v = np.zeros(x.size, dtype=x.dtype)
+    for i,n in enumerate(np.nditer(x)):
+        v[i] = n
+    print(v)
+    #print('['+('  '.join(str(n) for n in np.nditer(x)))+']')
 
 def meters(feet):
     return int(feet / 3.2808)
